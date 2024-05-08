4ed8650a30e0b206ee1de98fcaeece8eb38c5593
code-cyber-arch <archabdulrm@gmail.com>
2024-05-08 14:39:00 +1000

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split(" ")
-str = str[5] + " " + str[6] + " " + str[-4] + " " + str[0]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
