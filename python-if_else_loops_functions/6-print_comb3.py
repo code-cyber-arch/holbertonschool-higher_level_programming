278147ef069ed2be613316a95b6510e9edc2edb2
code-cyber-arch <archabdulrm@gmail.com>
2024-05-09 15:06:00 +1000

    6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j), end="\n")
+        print("{}{}".format(i, j), end=", ")
