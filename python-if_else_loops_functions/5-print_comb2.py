f2e54db731f33d481c42a0e10669a00f94412a32
code-cyber-arch <archabdulrm@gmail.com>
2024-05-09 14:55:42 +1000

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(f"{i}", end="\n")
-    print(f"{i:02d}", end=", ")
+        print("{:02d}".format(i), end="\n")
+    else:
+        print("{:02d}".format(i), end=", ")
