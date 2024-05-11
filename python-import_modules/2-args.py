fe5eb700aa22c1f9f63820b5b7456af67b723ce9
cod-cyber-arch <archabdulrm@gmail.com>
2024-05-11 02:01:05 +0000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     import sys
 
     arg = len(sys.argv) - 1
-    print("{} arguments:".format(arg))
+    if arg == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(arg))
 
     for i in range(arg):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
