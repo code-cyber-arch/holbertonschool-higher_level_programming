2000dfa141eb9d7c865f31e9d0849e77dde8ed5b
code-cyber-arch <archabdulrm@gmail.com>
2024-05-12 09:43:09 +1000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         list_a.append(0)
     while len(list_b) < 2:
         list_b.append(0)
-    list_c = [0,0]
+    list_c = [0, 0]
     for i in range(2):
         list_c[i] = list_a[i] + list_b[i]
     return tuple(list_c)
