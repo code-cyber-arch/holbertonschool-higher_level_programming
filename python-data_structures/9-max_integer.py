4e556487a195f4198206e37fac2d2a5d114ea842
code-cyber-arch <archabdulrm@gmail.com>
2024-05-12 11:23:26 +1000

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) > 0:
+        max_val = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max_val:
+                max_val = my_list[i]
+        return max_val
