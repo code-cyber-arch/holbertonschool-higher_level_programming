8c1951fc76671451cff02c183b9e25e358d84345
code-cyber-arch <archabdulrm@gmail.com>
2024-05-13 15:17:43 +1000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,13 @@
 
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return_num = 0
     for i in roman_string:
-        if i in roman.keys():
-            if return_num > roman[i]:
-                return_num += roman[i]
-            else:
-                return_num -= roman[i]
-                return_num = abs(return_num)
+        if i not in roman:
+            return None
+    return_num = roman[roman_string[-1]]
+    for i in range(len(roman_string) - 2, -1, -1):
+        if roman[roman_string[i]] >= roman[roman_string[i + 1]]:
+            return_num += roman[roman_string[i]]
         else:
-            break
+            return_num -= roman[roman_string[i]]
     return return_num
