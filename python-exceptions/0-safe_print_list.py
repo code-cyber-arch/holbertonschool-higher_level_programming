ea87aad1ea5307285284223fd100e075ebc5a803
code-cyber-arch <archabdulrm@gmail.com>
2024-05-14 09:31:15 +1000

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    output =[]
+    for i in range(x):
+        try:
+            output.append(str(my_list[i]))
+            count += 1
+        except IndexError:
+            break
+    print("{}".format("".join(output)))
+    return count
