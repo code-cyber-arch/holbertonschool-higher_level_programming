0765a1d2547d82d1f1df92ad2bda879d5573f834
code-cyber-arch <archabdulrm@gmail.com>
2024-05-18 17:14:35 +1000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,10 +27,11 @@ class Square:
         if self.__size == 0:
             print('')
         else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print('#', end="")
-                print('')
+            for _ in range(self.__position[1]):
+                print("")
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
 
     @property
     def position(self):
