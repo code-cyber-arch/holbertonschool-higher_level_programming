8c524fbabcf9cfd74ecc74197262c079a18995b4
code-cyber-arch <archabdulrm@gmail.com>
2024-05-18 14:59:18 +1000

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0) -> None:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
+
+    def area(self):
+        return self.__size ** 2
