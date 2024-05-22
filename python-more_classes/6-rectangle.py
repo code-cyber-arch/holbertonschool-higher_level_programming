61c1558c9a30acb4a3d3d3b31f243e075f37b630
code-cyber-arch <archabdulrm@gmail.com>
2024-05-22 13:04:16 +1000

    How many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,9 +3,10 @@
 
 
 class Rectangle:
+    """Represent a rectangle."""
 
     number_of_instances = 0
-    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
