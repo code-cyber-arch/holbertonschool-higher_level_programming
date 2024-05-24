47996fc15cd9c058a9869e7c1e09c040b4857c9e
code-cyber-arch <archabdulrm@gmail.com>
2024-05-24 19:39:04 +1000

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Define a class."""
 
+
 class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-
         """print list."""
         sorted_list = sorted(self)
         print(sorted_list)
