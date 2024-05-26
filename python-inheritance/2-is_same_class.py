18492d3ba83aca5008fc2c85852149206e57a27e
code-cyber-arch <archabdulrm@gmail.com>
2024-05-27 09:03:41 +1000

    2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Function that returns True/False if obj is a type of a_class"""
+
 
 def is_same_class(obj, a_class):
     """Function that returns True/False if obj is a type of a_class
