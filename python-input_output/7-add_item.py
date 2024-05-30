336d401a08981a1e4b1368335642cdbde6cea3ad
code-cyber-arch <archabdulrm@gmail.com>
2024-05-30 12:19:38 +1000

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+import os
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
+    if os.path.exists(filename):
+        items = load_from_json_file(filename)
+    else:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
