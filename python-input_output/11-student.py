559c5738cfc0f861f52de76b7250aaf9b974d427
code-cyber-arch <archabdulrm@gmail.com>
2024-05-30 14:48:07 +1000

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" class Student that defines a student"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student
+            instance based on specified attributes.
+
+        Args:
+            attrs (list, optional): A list of attribute names to
+            include in the result.
+
+        Returns:
+            dict: A dictionary representation of the instance.
+        """
+        if isinstance(attrs, list):
+            return {attr: getattr(self, attr, None) for
+                    attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance with values
+            from the provided JSON dictionary.
+
+        Args:
+            json (dict): A dictionary where keys are attribute names
+            and values are the values for those attributes.
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
