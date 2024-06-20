7b6352e05d68c6119c9b784a34bd653b87838c07
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 17:11:57 +1000

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all
+    values in the states table of hbtn_0e_0_usa where
+    name matches the argument."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    state_name = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE BINARY name = '{}'".format(
+        state_name))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
