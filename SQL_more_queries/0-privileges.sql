8f078c1c9920aae2e11df13ca1d6c1c3cd1ac37e
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 10:28:48 +1000

    Privilege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- List all privileges
 SHOW GRANTS
-    FOR'user_0d_1'@'localhost';
+    FOR 'user_0d_1'@'localhost';
 SHOW GRANTS
-    FOR'user_0d_1'@'localhost';
+    FOR 'user_0d_2'@'localhost';
