c7f511bf610887703ce04e660e2329f0dc8db144
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 11:45:06 +1000

    6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database and table with auto generate id
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE
+    IF NOT EXISTS hbtn_0d_usa.states (
+        id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+        name VARCHAR(256) NOT NULL
+    );
