e87f4625c0dd5daecfd1e0b3f74a72061fdaa0f7
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 16:03:13 +1000

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         db=sys.argv[3],
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name = %s", (sys.argv[4],))
+    cur.execute("SELECT * FROM `states` WHERE name = '{}'".format(sys.argv[4],))
     states = cur.fetchall()
     for state in states:
         print(state)
