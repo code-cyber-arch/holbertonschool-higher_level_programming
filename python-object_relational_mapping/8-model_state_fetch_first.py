76bf1659a6eef2f411e6d8977e64eb9e6849135a
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 18:47:15 +1000

    8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    print('Nothing' if not state else "{}: {}".format(state.id, state.name))
