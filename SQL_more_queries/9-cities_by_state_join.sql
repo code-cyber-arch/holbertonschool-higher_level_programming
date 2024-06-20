37868f2004c02d25daa4d8b71a1a3f0dab207f8f
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 12:40:56 +1000

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,5 @@
 --  List all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, (
-    SELECT states.name
-    FROM states
-    WHERE states.id = cities.state_id
-) AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-ORDER BY cities.id;
+JOIN states
+    ON cities.state_id = states.id;
