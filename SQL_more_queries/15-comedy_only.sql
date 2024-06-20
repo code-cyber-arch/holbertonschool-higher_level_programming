c7c1066b94ab1c711a6f20d0c5045a91df8ba518
code-cyber-arch <archabdulrm@gmail.com>
2024-06-20 14:17:31 +1000

    15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@ FROM tv_shows
 JOIN tv_show_genres
     ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
-    ON tv_genres.id = tv_show_genres.show_id
-WHERE tv_genres.name = 'Comedy'
+    ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
