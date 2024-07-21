3cd8520979505eb1aeb467a7745a46afe4a5e6be
Abdurahman Mahammedsied <52357054+code-cyber-arch@users.noreply.github.com>
2024-07-21 21:04:04 +1000

    Update task_02_logic.py

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-"""Render HTML pages"""
-import json
+
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    """Render home page"""
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    """Render about page"""
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    """Render contact page"""
-    return render_template('contact.html')
-
 @app.route('/items')
-def show_items():
-    """Render items page"""
-    try:
-        with open('items.json', 'r') as file:
-            items_data = json.load(file)
-            items = items_data.get('items', [])
-    except FileNotFoundError:
-        items = []
-    
-    return render_template('items.html', items=items)
+def items():
+    items_list = []
 
+    with open("items.json", 'r') as f:
+        rows = json.load(f)
+    for key,value in rows.items():
+        items_list = value
+
+    return render_template('items.html', items=items_list)
+
+# Set debug=True for the server to auto-reload when there are changes
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='localhost', port=5000, debug=True)
